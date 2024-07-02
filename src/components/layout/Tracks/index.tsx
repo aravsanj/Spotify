5e979d0053855fcacff71afbc55fbb7c9b6c769e
Aravind Sanjeev <saravind436@gmail.com>
2024-07-02 20:03:15 +0530

    fix: broken see all button on phone

--- src/components/layout/Tracks/index.tsx
+++ src/components/layout/Tracks/index.tsx
@@ -4,9 +4,9 @@ import TrackList from "./Tracks";
 export default function Tracks() {
   return (
     <div className="flex w-full flex-col gap-y-7 px-6 md:px-4 lg:px-10">
-      <div className="flex items-center justify-between">
+      <div className="flex flex-col justify-between sm:flex-row sm:items-center">
         <span className="text-2xl font-bold text-white">Your top tracks</span>
-        <span className="text-white">
+        <span className="text-sm text-white">
           <Link href="/tracks">SEE ALL</Link>
         </span>
       </div>
